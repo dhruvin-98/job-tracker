d29cdfc1a56c7d6987793b3a113312baf1ef01b4
dhruvin <dhruvinborad1910@gmail.com>
2026-03-26 20:35:03 +0530

    add frontend files

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar'
+import JobCard from '../components/JobCard'
+
+function Dashboard() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Dashboard</h1>
+      <JobCard />
+    </div>
+  )
+}
+
+export default Dashboard
